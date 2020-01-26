867fbfb20a54e4ca5e5cf2ee5ac38c3fe1db2271
William R. Fraser <wfraser@codewise.org>
2020-01-26 14:57:45 -0800

    silence a bunch of lints in libzfs-sys

--- libzfs-sys/src/lib.rs
+++ libzfs-sys/src/lib.rs
@@ -1,5 +1,13 @@
+// silence style lints and things that don't actually matter
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::redundant_static_lifetimes)]
+#![allow(clippy::trivially_copy_pass_by_ref)]
+#![allow(clippy::unreadable_literal)]
+#![allow(clippy::useless_transmute)]
+
+// this should actually be fixed, but it's not clear how: some functions have u128 return types
+#![allow(improper_ctypes)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
